5b3e24a400fb15f65219d43caed45e272a056a80
frankpettersson <frank.pettersson1@gmail.com>
2019-05-20 21:10:48 +0200

    crol

---
+++ projects/graphics/paper.js/langtonsAnt/main.js
@@ -0,0 +1,5 @@
+var container = document.getElementById('container');
+var canvasWidth = 400;
+var canvasHeight = 400;
+container.style.width = canvasWidth+'px';
+container.style.height = canvasHeight+'px';
