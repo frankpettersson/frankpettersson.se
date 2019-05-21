c54b3b2f786e6f4e77fa3cefa2fc38ff32aed991
Frank <frankpettersson1@gmail.com>
2019-05-21 09:23:30 +0200

    ye

--- projects/games/units/main.js
+++ projects/games/units/main.js
@@ -36,7 +36,7 @@ let root = {
         this.setValue(unitCreates, this.lastUnit);
         this.setValue(unitOwned, owned);
 
-        //append table entries to table
+        //append table entries to tables
         unit.appendChild(unitName);
         unit.appendChild(unitCreates);
         unit.appendChild(unitOwned);
